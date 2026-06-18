5ca7798b3bd559ea96d7e03f85579abccb0cf91e
Ubuntu <ubuntu@ip-10-0-0-18.ap-south-1.compute.internal>
2026-06-18 09:14:21 +0000

    Initial Terraform project

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr = "192.168.0.0/16"
+  vpc_name = "prod-vpc"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+  instance_name = "prod-server"
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = "manoj-prod-bucket-12345"
+}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "bucket_name" {
+  value = module.s3.bucket_name
+}
